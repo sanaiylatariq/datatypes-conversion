73da4a3236ddd6eaf1e0fb4226b97baa7bd301c6
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-06-26 14:20:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//let sunny = "333abc"
+//let valueinNumber = Number(sunny)
+//console.log(valueinNumber);
+//console.log(typeof sunny);
+
+//NOTES for valueinNumber
+//"333" = 333,
+ //"333abc"  = notanumber or NAN,
+ // true = 1;false = 0,
+
+//let isLoggedIn = 1
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+//let isLoggedOut = 0
+//let booleanisLoggedOut = Boolean(isLoggedOut)
+//console.log(booleanisLoggedOut);
+//let f = "1"
+///let sunny = Boolean(f)
+//console.log(typeof sunny);
+//let ashee = 33
+//let stringno = String(ashee)
+//console.log(stringno);
+//console.log(typeof stringno);
+
+let ashee = "000"
+let stringno = Object(ashee)
+console.log(stringno);
+console.log(typeof stringno);
+//jo 2nd let mn aye ga wo function hoga yani number likho to whi, string likho to wohi ,boolean likho to true ya false nature/typeof mn type btae ga k string hay ya boolean ya number
---
+++ sunny.js
@@ -0,0 +1,8 @@
+
+let value = "22"
+let values = value
+console.log(typeof value);
+let a = "1"
+let hard = Boolean(a)
+console.log(hard);
+console.log(typeof hard);
